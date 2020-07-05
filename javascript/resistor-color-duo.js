313a91b600a824502c67bdbaf75ea893dbdcad58
agrymaloski <48161305+agrymaloski@users.noreply.github.com>
2020-07-05 13:49:57 -0700

    solved resister-color-duo.js
    This is a solution to the problem defined below. This was taken from Exercism.io under the javascript section.
    
    If you want to build something using a Raspberry Pi, you'll probably use resistors. For this exercise, you need to know two things about them:
    
    Each resistor has a resistance value.
    Resistors are small - so small in fact that if you printed the resistance value on them, it would be hard to read.
    To get around this problem, manufacturers print color-coded bands onto the resistors to denote their resistance values. Each band has a position and a numeric value. For example, if they printed a brown band (value 1) followed by a green band (value 5), it would translate to the number 15.
    
    In this exercise you are going to create a helpful program so that you don't have to remember the values of the bands. The program will take color names as input and output a two digit number, even if the input is more than two colors!
    
    The colors are mapped to the numbers from 0 to 9 in the sequence: Black - Brown - Red - Orange - Yellow - Green - Blue - Violet - Grey - White
    
    From the example above: brown-green should return 15 brown-green-violet should return 15 too, ignoring the third color.

---
+++ javascript/resistor-color-duo.js
@@ -0,0 +1,14 @@
+const color_dict = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export const decodedValue = ([color1, color2]) => color_dict[color1] * 10 + color_dict[color2];
